1ff54b454852fdb854c1404d88d354c217a13914
thampaponn <88426179+thampaponn@users.noreply.github.com>
2024-03-08 15:40:57 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const aws = require('aws-sdk');
-  aws.config = new aws.Config();
-  aws.config.accessKeyId = "ASIAVRUVQ44QK74ZFEHP";
-  aws.config.secretAccessKey = "vvQ2fRWkEUc1LTtIA339xdbHeagxfcyCBWtFYltv";
-  aws.config.region = "us-east-1";
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
